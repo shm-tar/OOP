b391a91ac7c00d433e29a106620ca684b1437329
Taras Shmilyk <31892672+taras-shmilyk@users.noreply.github.com>
2018-03-20 19:14:37 +0200

    Create ShellSort.java

---
+++ Lab 2/ShellSort.java
@@ -0,0 +1,15 @@
+public class ShellSort {
+
+	public static void main(String[] args) {
+		int[] array = {30, 2, 10, 4, 6};
+		int length = array.length;
+
+		//PUT YOUR CODE HERE
+
+		//PUT YOUR CODE HERE
+	    
+		for (int i = 0; i < length; i++) {
+			System.out.print(array[i] + " ");
+		}
+	}
+}
