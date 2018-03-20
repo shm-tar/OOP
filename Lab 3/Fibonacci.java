81f81b9e4235f50ea87d77261f6f94882457ba5e
Taras Shmilyk <31892672+taras-shmilyk@users.noreply.github.com>
2018-03-20 20:05:49 +0200

    Update Fibonacci.java

--- Lab 3/Fibonacci.java
+++ Lab 3/Fibonacci.java
@@ -3,17 +3,12 @@ package com.tasks3.fibonacci;
 public class Fibonacci {
 	
 	public long getNumber(int position) {
-		if (position == 1) {
-			return 0;
+		if (position < 1) {
+			return -1L;
 		}
-		else if (position == 2) {
-			return 1;
+		else if (position == 1 || position == 2) {
+			return 1L;
 		}
-		else if (position >= 3) {
-			return getNumber(position-1)+getNumber(position-2);
-		}
-		else {
-			return -1;
-		}
+		else return getNumber(position-1)+getNumber(position-2);
 	}
 }
