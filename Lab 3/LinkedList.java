de711c97d8aeab6895bc6c8dc413de8e237f54bc
Taras Shmilyk <31892672+taras-shmilyk@users.noreply.github.com>
2018-03-20 19:26:47 +0200

    Update LinkedList.java

--- Lab 3/LinkedList.java
+++ Lab 3/LinkedList.java
@@ -1,1 +1,28 @@
+package com.tasks3.linkedlist;
 
+public class LinkedList {
+	public LinkedList() {
+		//PUT YOUR CODE HERE
+		//PUT YOUR CODE HERE
+	}
+
+	public void add(Integer data) {
+                //PUT YOUR CODE HERE
+                //PUT YOUR CODE HERE
+	}
+
+	public Integer get(int index) {
+                //PUT YOUR CODE HERE
+                //PUT YOUR CODE HERE
+	}
+
+	public boolean delete(int index) {
+                //PUT YOUR CODE HERE
+                //PUT YOUR CODE HERE
+	}
+
+	public int size() {
+                //PUT YOUR CODE HERE
+                //PUT YOUR CODE HERE
+	}
+}
