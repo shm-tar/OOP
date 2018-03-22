b625c51419145b6fba1bbee9ed2ac3f3f6091c80
Taras Shmilyk <31892672+taras-shmilyk@users.noreply.github.com>
2018-03-22 16:58:41 +0200

    Update ShellSort.java

--- Lab 2/ShellSort.java
+++ Lab 2/ShellSort.java
@@ -1,37 +1,30 @@
 class Shell{
-public static void main(String[] args){
+    public static void main(String[] args){
 
-int[] array = {30, 2, 10, 4, 6};
-for(int i=0; i < array.length; i++){
-	System.out.print(array[i]);
-	System.out.print(' ');
-}
-System.out.println();
+        int[] array = {30, 2, 10, 4, 6};
+        int n = array.length;
+        for(int i=0; i < array.length; i++){
+            System.out.print(array[i]);
+            System.out.print(' ');
+        }
+        System.out.println();
 
-shell(array);
+        shell(array);
 
-}
-public static void shell(int[] a) {
-	int increment = a.length / 2;
-	while (increment > 0) {
-		for (int i = increment; i < a.length; i++) {
-			int j = i;
-			int temp = a[i];
-			while (j >= increment && a[j - increment] > temp) {
-				a[j] = a[j - increment];
-				j = j - increment;
-			}
-			a[j] = temp;
-		}
-		if (increment == 2) {
-			increment = 1;
-		} else {
-			increment *= (5.0 / 11);
-		}
-	}
-	for(int i=0; i < a.length; i++){
-		System.out.print(a[i]);
-		System.out.print(' ');
-	}
-}
+    }
+    public static void shell(int[] a) {
+        int n = a.length;
+        for (int gap = n/2; gap > 0; gap /= 2)
+        {
+            for (int i = gap; i < n; i += 1)
+            {
+                int temp = a[i];
+                int j;
+                for (j = i; j >= gap && a[j - gap] > temp; j -= gap)
+                    a[j] = a[j - gap];
+
+                a[j] = temp;
+            }
+        }
+    }
 }
