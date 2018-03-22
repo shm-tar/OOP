89b7392887c975851525f10a92871d3047309b6b
Taras Shmilyk <31892672+taras-shmilyk@users.noreply.github.com>
2018-03-22 12:13:04 +0200

    Update LinkedList.java

--- Lab 3/LinkedList.java
+++ Lab 3/LinkedList.java
@@ -1,28 +1,65 @@
 package com.tasks3.linkedlist;
 
 public class LinkedList {
-	public LinkedList() {
-		//PUT YOUR CODE HERE
-		//PUT YOUR CODE HERE
-	}
+    
+    private Node start;
+    private Node end;
+    private int size = 0;
 
-	public void add(Integer data) {
-                //PUT YOUR CODE HERE
-                //PUT YOUR CODE HERE
-	}
+    public LinkedList() {
 
-	public Integer get(int index) {
-                //PUT YOUR CODE HERE
-                //PUT YOUR CODE HERE
-	}
+    }
 
-	public boolean delete(int index) {
-                //PUT YOUR CODE HERE
-                //PUT YOUR CODE HERE
-	}
+    public void add(Integer data) {
+        Node newNode = new Node();
+        newNode.setData(data);
 
-	public int size() {
-                //PUT YOUR CODE HERE
-                //PUT YOUR CODE HERE
-	}
+        if (size == 0)
+            start = newNode;
+        else
+            end.setNext(newNode);
+
+        end = newNode;
+        size++;
+    }
+
+    public Integer get(int index) {
+        return findNodeByIndex(index).getData();
+    }
+
+    public boolean delete(int index) {
+        if (findNodeByIndex(index) != null ) {
+
+            if (index != 0)
+                findNodeByIndex(index - 1).setNext(findNodeByIndex(index+1));
+            else
+                start = start.getNext();
+
+            size--;
+            return true;
+        }
+        return false;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    private Node findNodeByIndex(int index) {
+        if (index < size && index >= 0) {
+            Node curNode = start;
+            int curIndex = 0;
+            while (curIndex < index){
+                if (curNode.getNext() != null) {
+                    curNode = curNode.getNext();
+                    curIndex++;
+                }
+
+            }
+            return curNode;
+        }
+        else
+            return null;
+    }
+
 }
