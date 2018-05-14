a973ceb2522925482d334aee0d1962a9381beff3
Taras Shmilyk <31892672+taras-shmilyk@users.noreply.github.com>
2018-05-14 15:03:05 +0300

    Create Main.java

---
+++ Lab10/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        TicketOffice information = new TicketOffice("Lviv Railway Station", "20:10", "20:12", "4",
+                "57", "Fridays", "54E67");
+        System.out.println(information);
+    }
+}
